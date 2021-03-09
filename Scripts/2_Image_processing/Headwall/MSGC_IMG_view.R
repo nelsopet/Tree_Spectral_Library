9622170c4abcb25df21b2b4b535a6332de953d66
nelsopetgit configgit config --global user.name nelsopet <peter.ross.nelson@gmail.com>
2021-03-09 13:39:17 -0500

    can get extent of all files in a directory ending in ENVI format

--- Scripts/2_Image_processing/Headwall/MSGC_IMG_view.R
+++ Scripts/2_Image_processing/Headwall/MSGC_IMG_view.R
@@ -4,7 +4,9 @@ require(tidyverse)
 require(hsdar)
 require(sf)
 require(mapview)
-
+require(caTools)
+require(terra)
+require(tools)
 #View images
 tst<-brick("./Original_data/Headwall/MSGC_TST_IMG")
 plotRGB(tst, r=160, g=80, b=25, stretch="lin")
--- Scripts/2_Image_processing/Headwall/MSGC_extent.R
+++ Scripts/2_Image_processing/Headwall/MSGC_extent.R
@@ -1,27 +1,38 @@
 #Get extent of an object
 
-
-#Get all the images in a directory for which extents are needed
-stuff<-list_files_with_exts(dir="./Original_data/Headwall/", exts = "hdr")
-
-
 #UNIT TEST: get_extents<- function(path)
-#path<-"./Original_data/Headwall/MSGC_TST_IMG"
+path<-"./Original_data/Headwall/MSGC_TST_IMG"
+path2<-"./Original_data/Headwall/"
+  files<-list.files(path2)
+  filenames<- subset(files,grepl(".hdr",files)==TRUE|grepl(".HDR",files)==TRUE)
+  filenames<-file_path_sans_ext(filenames)  
+  fileread<-lapply(1:length(filenames), function(x) {raster(paste("./Original_data/Headwall/",filenames[x],sep=""))})  
+  ext_out<-lapply(fileread, extent)
+  
+raster(path) %>% extent()
 tst<-brick(path)
 tst_proj<-proj4string(tst)
 tst_extent<-extent(tst) %>% as("SpatialPolygons")
 proj4string(tst_extent)<-tst_proj
-#mapview(tst_extent)
+
 
 get_extents<- function(path)
 {
-tst<-brick(path)
-tst_proj<-proj4string(tst)
-tst_extent<-extent(tst) %>% as("SpatialPolygons")
-proj4string(tst_extent)<-tst_proj
-return(tst_extent)
+  files<-list.files(path)
+  filenames<- subset(files,grepl(".hdr",files)==TRUE|grepl(".HDR",files)==TRUE)
+  filenames<-file_path_sans_ext(filenames)  
+  fileread<-lapply(1:length(filenames), function(x) {raster(paste(path,filenames[x],sep=""))})  
+  ext_out<-lapply(fileread, extent)
+  #ext_out<-lapply(ext_out, as("SpatialPolygons"))
+  return(ext_out)
 }
 
-lapply(stuff,get_extents)
+file_extents<-lapply(path2,get_extents)
 
+file_extents<-unlist(file_extents)
 
+file_extents<-lapply(1:length(file_extents), function(x){ as(file_extents[[x]],"SpatialPolygons")})
+
+str(file_extents)
+
+##Need to assign CRS to these
