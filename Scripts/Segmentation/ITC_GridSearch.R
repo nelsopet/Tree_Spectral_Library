ef6a92922c0489a1fae39f2ef8a9fe787e7be535
krbundy <kenneth.bundy@maine.edu>
2021-07-19 14:06:30 -0400

    Image Segmentation test

--- Scripts/Segmentation/ITC_GridSearch.R
+++ Scripts/Segmentation/ITC_GridSearch.R
@@ -4,75 +4,140 @@ require(sp)
 require(raster)
 require(itcSegment)
 
-# assumes that the file is projected and ready to go
+# load and project the target image
+
+# plan: finish this and return to original plan
+# reduce image size to something smaller with a few segments
+# 
+# Theme 4 cross-polination w/ theme 1
+# week of July 19
+
+
+# get relevant CRS
+epsg_code <- 26983
+target_wkt <- sf::st_crs(epsg_code)[[2]]
+print(target_wkt)
+target_crs <- sp::CRS(target_wkt)
+print(target_crs)
+
+# define tile files for the test
+base_tile_path <- paste0(
+    getwd(),
+    "/Data/raw_15366_rd_rf_or/Tiles/A_001_raw_rd_rf_or_Tile_"
+    )
+tile_paths <- c(
+    paste0(base_tile_path, 16, ".envi"),
+    paste0(base_tile_path, 17, ".envi"),
+    paste0(base_tile_path, 18, ".envi"),
+    paste0(base_tile_path, 19, ".envi"),
+    paste0(base_tile_path, 20, ".envi")
+)
+
+# define function to merge the tiles and extract the correct band
+merge_tiles <- function(input_files, output_path = NA) {
+
+    master_raster <- as(raster::brick(input_files[[1]])[[225]], "RasterLayer")
+        print("Extent of first tile:")
+        print(raster::extent(master_raster))
+
+    for (input_file in tail(input_files, -1)) {
+        new_raster <- as(raster::brick(input_file)[[225]], "RasterLayer")
+        print("Extent of tile:")
+        print(raster::extent(new_raster))
+        master_raster <- raster::merge(
+            master_raster,
+            new_raster,
+            tolerance = 0.5)
+        
+    }
+    if(!is.na(output_path)) {
+        raster::writeRaster(master_raster, output_path)
+    }
+
+    return(master_raster)
+}
+
+# load the data from the relevant tiles
+test_raster <- merge_tiles(tile_paths)
+
+image_data <- raster::projectRaster(
+    master_raster,
+    crs = target_crs,
+    method = "bilinear")
 
 
 
 # should pre-load dataset as input_data object
 epsg <- 26983
 input_file_path <- "Data/Projected_Test_Raster"
-image_data <- raster::rasterStack(input_file_path)
+#image_data <- raster::rasterStack(input_file_path)
+shapefile_path <- "Data/Shapefiles/15366_crown_delins.shp"
 
-ash_seg_path <- "Data/Segmentation"
-ash_segs <- rgdal::readOGR(ash_seg_path)
+human_tree_shapes <- rgdal::readOGR(
+    dsn = shapefile_path,
+    layer = "15366_crown_delins",
+)
+
+transformed_trees <- sp::spTransform(
+    human_tree_shapes, target_crs)
+
+cropped_shape <- raster::crop(
+    transformed_trees,
+    raster::extent(transformed_master_raster))
 
 save_directory <- "Outputs/ImageSegmentationSearch/"
 thresholds <- c(0.4, 0.5, 0.6)
 threshold_delta <- c(0.05, 0.1, 0.15)
-distances <- c(10, 20,  30)
-windows_sizes <- c(11, 27, 35, 43, 51)
+distances <- c(10, 20, 30)
+window_sizes <- c(11, 35, 51)
+# good question: other wavelengths;
+# shelve for later
+# could do gradient descent on linear functional from spectra to model input
+# regression 
+# st_transform (vector-based; shapefile)
+# terra <- R raster-like package
 
-for (thresh in thresholds) {
-    for (delta in threshold_delta) {
-        for (dist in distances) {
-            for (window in window_sizes) {
-                # create segmentation 
-                segments <- itcSegment::itcIMG(
-                    imagery = image_data,
-                    epsg = 26983,
-                    searchWinSize = window,
-                    THRESHSeed = thresh,
-                    THRESHCrown = (thresh + delta),
-                    DIST = dist
-                )
 
-                # create filename for the plot
-                plot_filename <- paste(
-                    save_directory,
-                    "img"
-                    window,
-                    thresh,
-                    delta,
-                    dist,
-                    ".png",
-                    sep = "_")
-                # save plot image
-                png(filename = plot_filename)
-                plot(image_data)
-                plot(segments, add = TRUE)
-                plot(ash_segs, add = TRUE)
-                dev.off()
+for (dist in distances) {
+    for (window in window_sizes) {
+        # create segmentation 
+        segments <- itcSegment::itcIMG(
+            imagery = image_data,
+            epsg = 26983,
+            searchWinSize = window,
+            DIST = dist
+        )
 
-                # name for the shapefile
-                shape_filepath <- paste(
-                    save_directory,
-                    "shp"
-                    window,
-                    thresh,
-                    delta,
-                    dist,
-                    sep = "_"
-                    )
-                # write to disk as ESRI shapefile.  
-                #See rgdal::ogrDrivers() for complete list of drivers/formats
-                rgdal::writeOGR(
-                    segments, 
-                    shape_filepath,
-                    "Segmentation",
-                    "ESRI Shapefile"
-                )
-            }
-        }
+        # create filename for the plot
+        plot_filename <- paste(
+            save_directory,
+            "img",
+            window,
+            dist,
+            ".png",
+            sep = "_")
+        # save plot image
+        png(filename = plot_filename)
+        plot(image_data)
+        plot(segments, add = TRUE)
+        plot(cropped_shape, add = TRUE)
+        dev.off()
+
+        # name for the shapefile
+        shape_filepath <- paste(
+            save_directory,
+            "shp",
+            window,
+            dist,
+            sep = "_"
+            )
+        # write to disk as ESRI shapefile.  
+        #See rgdal::ogrDrivers() for complete list of drivers/formats
+        rgdal::writeOGR(
+            segments, 
+            shape_filepath,
+            "Segmentation",
+            "ESRI Shapefile"
+        )
     }
-
 }
--- Scripts/Segmentation/itc_test.R
+++ Scripts/Segmentation/itc_test.R
@@ -3,12 +3,13 @@ require(sp)
 require(sf)
 
 library(tidyverse)
-library(raster)
-library(itcSegment)
+require(raster)
+require(itcSegment)
 
 
 # Load the data
-input_file_path <- "Data/MSGC_TST_IMG"
+#input_file_path <- "Data/MSGC_TST_IMG"
+input_file_path <- "Data/raw_15366_rd_rf_or/raw_15366_rd_rf_or_PredLayer.tif"
 
 data_brick <- raster::stack(input_file_path)
 data_crs <- raster::projection(data_brick, asText = FALSE)
@@ -18,7 +19,9 @@ print(data_crs)
 # select 812nm band & plot it
 layer_id <- 225
 print(names(data_brick[[layer_id]]))
-raster::plot(as(data_brick[[layer_id]], "RasterLayer"))
+plot(data_brick)
+
+
 
 # useful EPSG codes can be found in Data/useful_speg_codes.txt
 # this code is for base WGS84;
@@ -40,7 +43,7 @@ new_prj <- crs(new_prj)
 
 # project the data to the new CRS
 projected_raster <- raster::projectRaster(
-    data_brick[[layer_id]],
+    data_brick,
     crs = target_crs,
     method = "bilinear")
 
@@ -67,3 +70,89 @@ tree_segments <- itcSegment::itcIMG(
 
 plot(projected_raster)
 plot(tree_segments, add = TRUE)
+
+
+# load the shapefile
+shapefile_path <- "Data/Shapefiles/15366_crown_delins.shp"
+
+human_tree_shapes <- rgdal::readOGR(
+    dsn = shapefile_path,
+    layer = "15366_crown_delins",
+)
+
+transformed_trees <- sp::spTransform(
+    human_tree_shapes, target_crs)
+plot(transformed_trees, add = TRUE)
+
+merge_tiles <- function(input_files, output_path = NA) {
+
+    master_raster <- as(raster::brick(input_files[[1]])[[225]], "RasterLayer")
+        print("Extent of first tile:")
+        print(raster::extent(master_raster))
+
+    for (input_file in tail(input_files, -1)) {
+        new_raster <- as(raster::brick(input_file)[[225]], "RasterLayer")
+        print("Extent of tile:")
+        print(raster::extent(new_raster))
+        master_raster <- raster::merge(
+            master_raster,
+            new_raster,
+            tolerance = 0.5)
+        
+    }
+    if(!is.na(output_path)) {
+        raster::writeRaster(master_raster, output_path)
+    }
+
+    return(master_raster)
+}
+
+base_tile_path <- paste0(
+    getwd(),
+    "/Data/raw_15366_rd_rf_or/Tiles/A_001_raw_15366_rd_rf_or_Tile_"
+    )
+tile_paths <- c(
+    paste0(base_tile_path, 15, ".envi"),
+    paste0(base_tile_path, 16, ".envi"),
+    paste0(base_tile_path, 17, ".envi"),
+    paste0(base_tile_path, 18, ".envi"),
+    paste0(base_tile_path, 19, ".envi"),
+    paste0(base_tile_path, 20, ".envi")
+)
+
+master_raster <- merge_tiles(tile_paths)
+raster::writeRaster(master_raster, "merged_test_tiles_15366", overwrite = TRUE)
+
+
+base_tile_path <- paste0(
+    getwd(),
+    "/Data/raw_15366_rd_rf_or/Tiles/A_001_raw_rd_rf_or_Tile_"
+    )
+tile_paths <- c(
+    paste0(base_tile_path, 16, ".envi"),
+    paste0(base_tile_path, 17, ".envi"),
+    paste0(base_tile_path, 18, ".envi"),
+    paste0(base_tile_path, 19, ".envi"),
+    paste0(base_tile_path, 20, ".envi")
+)
+
+dev.off()
+plot(master_raster)
+
+transformed_master_raster <- raster::projectRaster(
+    master_raster,
+    crs = target_crs,
+    method = "bilinear")
+
+
+cropped_shape <- raster::crop(
+    transformed_trees,
+    raster::extent(transformed_master_raster))
+
+plot(transformed_master_raster)
+plot(cropped_shape, add = TRUE)
+
+raster::writeRaster(
+    transformed_master_raster,
+    "merged_test_tiles_projected")
+
