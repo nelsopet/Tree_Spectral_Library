42a9481907b092387a98ae48c32be8a91a5b5097
nelsopet <peter.ross.nelson@gmail.com>
2022-08-22 16:57:25 -0400

    added notes about images for canopy delineation

--- Scripts/2_Image_processing/Headwall/MSGC_get_trees.R
+++ Scripts/2_Image_processing/Headwall/MSGC_get_trees.R
@@ -20,10 +20,19 @@ flight_extents <- st_read(paste(extents_path, "/", "PEF_all.kml", sep = ""))
 
 
 #bring in all point data for tree stem maps
+#WHOLE DIRECTORY
 #These must be hand-checked to ensure the data is appropriate!
 treelist1 <- list.files(trees_path, recursive = T, pattern = ".shp")
 treelist <- subset(treelist1, grepl(".shp.", treelist1)==FALSE)
 treelist <- treelist[c(1,2,3,6,7,8,9)] 
+
+#SINGLE FILE
+treelist <- st_read("M:\\MSGC_DATA\\Shapefiles for all areas (Elias)\\Howland\\Shawn\\Howland_Shawn_Tree_Locs2.shp")
+treelist <- st_transform(treelist, crs = st_crs(flight_extents))
+pts_in <- st_join(treelist, flight_extents, join = st_within)
+trees_in_sub <- pts_in[!is.na(pts_in$Name),]
+
+
 
 #read in stem maps, transform CRS to match flight extents, and compile into df
 trees_in <- data.frame()
@@ -42,15 +51,37 @@ tree_counts <- trees_in_sub %>% count(Name)
 tree_countsbyspecies <- trees_in_sub %>% count(Name, SP)
 
 #favorite images (based on image quality and occurrence of P. rubens)
-#100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_3965_rd_rf_or (191 total trees)
-#100044_PEF_SM_11b_110m_2019_06_15_15_26_52_Orthos_raw_16967_rd_rf_or (188 total trees)
-#100047_PEF_SM_12a_110m_2019_06_16_15_25_41_Orthos_raw_15568_rd_rf_or (45 total trees)
+#HOWLAND
+#100003_Howland_plot_1a_110m_2019_07_01_16_48_13_Orthos_raw_14552_rd_rf_or
+#100003_Howland_plot_1a_110m_2019_07_01_16_48_13_Orthos_raw_16552_rd_rf_or
+
+#PEF
+#100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_3965_rd_rf_or (decent red maple and maybe red spruce)
+#100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or (16RS and 7EH delineated 8-22-22)
+#100047_PEF_SM_12a_110m_2019_06_16_15_25_41_Orthos_raw_15568_rd_rf_or (smaller sample but looks good)
+#100299_PEF_SM_1_flight_2020_06_27_16_19_38_Orthos_raw_11194_rd_rf_or (decent)
+#1000044 has several good images 
+#100044_PEF_SM_11b_110m_2019_06_15_15_26_52_Orthos_raw_19597_rd_rf_or (several trees and good image)
+#100044_PEF_SM_11b_110m_2019_06_15_15_26_52_Orthos_raw_10410_rd_rf_or
+
+
+
+#bad images
+#HOWLAND
+#100003_Howland_plot_1a_110m_2019_07_01_16_48_13_Orthos_raw_11583_rd_rf_or (streaky image)
+#100003_Howland_plot_1a_110m_2019_07_01_16_48_13_Orthos_raw_13583_rd_rf_or (streaky)
+
+#PEF
+#All of 100032 is bad
+#100035 is bad
+#100044_PEF_SM_11b_110m_2019_06_15_15_26_52_Orthos_raw_16967_rd_rf_or (no trees in center of image)
+#100306_PEF_SM_1d_flight_2020_06_28_15_12_04_Orthos_raw_16684_rd_rf_or (lots of trees but poor illumination)
 
 #subset individual images
-image_100038_3965 <- subset(trees_in_sub, Name == "100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_3965_rd_rf_or")
+image_100038_9492 <- subset(trees_in_sub, Name == "100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_9492_rd_rf_or")
 
 #write to file
-st_write(image_100038_3965, paste0(extents_path, "points_100038_3965.shp"), driver = "ESRI Shapefile")
+st_write(image_100038_9492, paste0(extents_path, "points_100038_9492.shp"), driver = "ESRI Shapefile")
 
 
 
