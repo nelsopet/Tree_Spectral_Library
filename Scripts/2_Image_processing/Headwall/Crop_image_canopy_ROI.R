cb72c6a5fd46513b3886ce74a689bb824f908b12
nelsopet <peter.ross.nelson@gmail.com>
2022-10-27 16:34:30 -0400

    got crop script working and started progress on parse

--- Scripts/2_Image_processing/Headwall/Crop_image_canopy_ROI.R
+++ Scripts/2_Image_processing/Headwall/Crop_image_canopy_ROI.R
@@ -5,28 +5,68 @@
 library(terra)
 library(raster)
 library(rgdal)
-library(sf)
 library(hsdar)
+library(rgeos)
+library(sf)
 
 #Define file paths
 image_path = "M:\\MSGC_DATA\\PEF-Demerit\\Imagery\\100038_PEF_SM_4_110m_2019_06_18_15_16_13\\ortho\\raw_7492_rd_rf_or"
-canopies_path = "M:\\MSGC_DATA\\PEF-Demerit\\Digitize_Canopies\\100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"
+canopies_path = "M:\\MSGC_DATA\\PEF-Demerit\\Digitize_Canopies\\100038_7492\\"
+
+#bring in test image
+img <- brick(image_path)
 
 #bring in vector of canopies
-img <- brick(image_path)
-#canopies_vec<-st_read(canopies_path)
-canopies_vec <- readOGR(dsn = canopies_path)
+#canopies_vec <- readOGR(dsn = paste0(canopies_path, "100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"))
+canopies_full <- readOGR(dsn = paste0(canopies_path, "Full_100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"))
+canopies_light <- readOGR(dsn = paste0(canopies_path, "Light_100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"))
+canopies_shadow <- readOGR(dsn = paste0(canopies_path, "Shadow_100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"))
 
-
-
-lapply(1:length(canopies_vec),  
+#Now, crop and mask image by each canopy shapefile. Functions are split into
+  #full canopies, illuminated only, and shadowed only
+#FULL CANOPIES#
+lapply(1:length(canopies_full),  
        function(x) {
          tst_img <- brick(image_path)
          tst_names<-names(tst_img)
-         tst_quads<-canopies_vec[x,]
+         tst_quads<-canopies_full[x,]
          tst_crop <- raster::crop(tst_img, tst_quads)
          tst_mask <- raster::mask(tst_crop, tst_quads)
-         metadata(tst_mask)<-as.list(canopies_vec[x,]$CLASS_NAME)
+         metadata(tst_mask)<-as.list(canopies_full[x,]$CLASS_NAME)
          bandnames(tst_mask)<-tst_names
-         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Test_output\\", canopies_vec[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\", canopies_full[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
        })
+
+#ILLUMINATED CANOPIES ONLY#
+lapply(1:length(canopies_light),  
+       function(x) {
+         tst_img <- brick(image_path)
+         tst_names<-names(tst_img)
+         tst_quads<-canopies_light[x,]
+         tst_crop <- raster::crop(tst_img, tst_quads)
+         tst_mask <- raster::mask(tst_crop, tst_quads)
+         metadata(tst_mask)<-as.list(canopies_light[x,]$CLASS_NAME)
+         bandnames(tst_mask)<-tst_names
+         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\", canopies_light[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+       })
+
+#SHADOWED CANOPIES ONLY#
+lapply(1:length(canopies_shadow),  
+       function(x) {
+         tst_img <- brick(image_path)
+         tst_names<-names(tst_img)
+         tst_quads<-canopies_shadow[x,]
+         tst_crop <- raster::crop(tst_img, tst_quads)
+         tst_mask <- raster::mask(tst_crop, tst_quads)
+         metadata(tst_mask)<-as.list(canopies_shadow[x,]$CLASS_NAME)
+         bandnames(tst_mask)<-tst_names
+         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\", canopies_shadow[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+       })
+
+
+
+
+
+
+
+
--- Scripts/2_Image_processing/Headwall/Parse_ROI_canopy_spectra.R
+++ Scripts/2_Image_processing/Headwall/Parse_ROI_canopy_spectra.R
@@ -1,24 +1,56 @@
-#Parse metadata and add to each raster of a single PFT
+#Parse metadata and add to each raster of a single tree canopy
 library(raster)
+library(hsdar)
+library(stringr)
+
+source("M:\\lecospec\\lecospec\\Functions\\dataframe_operations.R")
+source("M:\\lecospec\\lecospec\\Functions\\type_conversion.R")
 
 
-path<-("M:\\MSGC_DATA\\PEF-Demerit\\Test_output\\")
-allfiles<-list.files(path) 
-imgs = grep(".envi$", allfiles, value = TRUE)
+#Set directory
+path <- ("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\")
+
+#list all .grd files 
+allfiles <- list.files(path) 
+imgs <- grep(".grd$", allfiles, value = TRUE)
+
+
+####TEST SPACE####
 band_path<-brick(paste(path,imgs[1], sep=""))
-band_count<-names(band_path) %>% length()
-band_names <- read.csv("./assets/bands.csv")$x[1:band_count] %>% as.vector() #need to add csv of bands
-band_count<-names(path) %>% length()
 
-BisonPFT_labeled<-lapply(1:length(imgs), function(x){ 
-  imgs_names<-str_match(imgs[x], "PFTs\\s*(.*?)\\s*.envi") %>%
+band_count<-names(band_path) %>% length()
+
+imgs_names<-str_match(imgs[1], "\\s*(.*?)\\s*.grd") %>%
+  as.data.frame() %>% 
+  dplyr::select(V2) %>% 
+  as.data.frame()
+
+tst<-brick(paste(path,imgs[1], sep=""))
+band_count<-names(tst) %>% length()
+df <- raster::rasterToPoints(tst) %>% 
+  as.data.frame()%>%
+  dplyr::select(-x,-y)
+
+#extract_bands won't work because I can't find the referenced function "convert_wavelength_strings"
+new_names<-extract_bands(df)
+
+df <- filter_bands(df)
+
+df <- df_to_speclib(df, type="spectrolab")
+
+####PARSE LOOP####
+Canopy_labeled<-lapply(1:length(imgs), function(x){ 
+  #extract individual canopy names
+  imgs_names<-str_match(imgs[x], "\\s*(.*?)\\s*.grd") %>%
     as.data.frame() %>% 
     dplyr::select(V2) %>% 
     as.data.frame()
   
+  #bring in image
   tst<-brick(paste(path,imgs[x], sep=""))
+  #count bands
   band_count<-names(tst) %>% length()
-  names(tst)<-band_names
+  #create df with bands and values
   df <- raster::rasterToPoints(tst) %>% 
     as.data.frame()%>%
     dplyr::select(-x,-y)
