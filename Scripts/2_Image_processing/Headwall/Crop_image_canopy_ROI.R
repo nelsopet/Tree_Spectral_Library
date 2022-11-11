d67e6dc86eac4e7c96dbeec87e5efb0025bd0861
nelsopet <peter.ross.nelson@gmail.com>
2022-11-11 13:20:33 -0500

    cropped more canopies from PEF and Howland

--- Scripts/2_Image_processing/Headwall/Crop_image_canopy_ROI.R
+++ Scripts/2_Image_processing/Headwall/Crop_image_canopy_ROI.R
@@ -9,17 +9,17 @@ library(rgeos)
 library(sf)
 
 #Define file paths
-image_path = "M:/MSGC_DATA/PEF-Demerit/Imagery/100047_PEF_SM_12a_110m_2019_06_16_15_25_41/Orthos/raw_15568_rd_rf_or"
-canopies_path = "M:\\MSGC_DATA\\PEF-Demerit\\Digitize_Canopies\\100047_15568\\"
+image_path = "M:/MSGC_DATA/Howland/Imagery/100006_Howland_plot_1b_110m_2019_07_01_18_04_21/Orthos/raw_2000_rd_rf_or"
+canopies_path = "M:/MSGC_DATA/Howland/Digitize_Canopies/100006_2000/"
 
 #bring in test image
 img <- brick(image_path)
 
 #bring in vector of canopies
 #canopies_vec <- readOGR(dsn = paste0(canopies_path, "100038_PEF_SM_4_110m_2019_06_18_15_16_13_ortho_raw_7492_rd_rf_or.shp"))
-canopies_full <- readOGR(dsn = paste0(canopies_path, "full_PEF_100047_15568.shp"))
-canopies_light <- readOGR(dsn = paste0(canopies_path, "light_PEF_100047_15568.shp"))
-canopies_shadow <- readOGR(dsn = paste0(canopies_path, "shadow_PEF_100047_15568.shp"))
+canopies_full <- readOGR(dsn = paste0(canopies_path, "full_Howland_100006_2000.shp"))
+canopies_light <- readOGR(dsn = paste0(canopies_path, "light_Howland_100006_2000.shp"))
+canopies_shadow <- readOGR(dsn = paste0(canopies_path, "shadow_Howland_100006_2000.shp"))
 
 #Now, crop and mask image by each canopy shapefile. Functions are split into
   #full canopies, illuminated only, and shadowed only
@@ -33,7 +33,7 @@ lapply(1:length(canopies_full),
          tst_mask <- raster::mask(tst_crop, tst_quads)
          metadata(tst_mask)<-as.list(canopies_full[x,]$CLASS_NAME)
          bandnames(tst_mask)<-tst_names
-         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\", canopies_full[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\Howland\\Cropped_ROIs_spectra\\", canopies_full[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
        })
 
 #ILLUMINATED CANOPIES ONLY#
@@ -46,7 +46,7 @@ lapply(1:length(canopies_light),
          tst_mask <- raster::mask(tst_crop, tst_quads)
          metadata(tst_mask)<-as.list(canopies_light[x,]$CLASS_NAME)
          bandnames(tst_mask)<-tst_names
-         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\", canopies_light[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\Howland\\Cropped_ROIs_spectra\\", canopies_light[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
        })
 
 #SHADOWED CANOPIES ONLY#
@@ -59,7 +59,7 @@ lapply(1:length(canopies_shadow),
          tst_mask <- raster::mask(tst_crop, tst_quads)
          metadata(tst_mask)<-as.list(canopies_shadow[x,]$CLASS_NAME)
          bandnames(tst_mask)<-tst_names
-         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\PEF-Demerit\\Cropped_ROIs_spectra\\", canopies_shadow[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
+         writeRaster(tst_mask, paste("M:\\MSGC_DATA\\Howland\\Cropped_ROIs_spectra\\", canopies_shadow[x,]$CLASS_NAME, sep=""), format = "raster", overwrite = TRUE)
        })
 
 
